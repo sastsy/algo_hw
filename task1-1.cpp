dec52f748cfba0aa5206788962644f713e683e93
sastsy <sas_tsy@mail.ru>
2022-11-08 20:25:46 +0300

    initial commit

--- task1-1.cpp
+++ task1-1.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <vector>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n;
+    int x;
+    std::cin >> n;
+    std::vector<int> a;
+
+    std::cin >> x;
+    a.push_back(x);
+
+    for (int i = 1; i < n; ++i) {
+        std::cin >> x;
+        a.push_back(x);
+        if ((a[i] < a[i - 1] && i % 2 != 0) || (a[i] > a[i - 1] && i % 2 == 0)) {
+            std::swap(a[i], a[i - 1]);
+        }
+    }
+
+    for (int element : a) {
+        std::cout << element << " ";
+    }
+
     return 0;
 }
