785dc92d4b99de9617d4cb3b9a1ac8a6b4f7ac33
sastsy <sas_tsy@mail.ru>
2022-11-29 22:17:15 +0300

    initial commit

--- lecture5/task5-3.cpp
+++ lecture5/task5-3.cpp
@@ -8,46 +8,69 @@ using namespace std;
 void generateVector(vector<int> &A, int n) {
     mt19937 mt(time(nullptr));
     for (int i = 0; i < n; ++i) {
-        A.push_back(mt() % 100);
+        A.push_back(mt() % 1000);
     }
 }
 
-void heapify(vector<int> &A, int k) {
-    int min;
-    int j;
-    for (int i = ((k + 1) / 2) - 1; i >= 0; --i) {
-        j = i;
-        min = j * 2 + 1;
-        while (j * 2 + 1 <= k && A[j] > A[min]) {
-            if (j * 2 + 2 <= k && A[j * 2 + 2] < A[j * 2 + 1]) {
-                min = j * 2 + 2;
-            }
-            else {
-                min = j * 2 + 1;
-            }
-            swap(A[j], A[min]);
-            j = min;
-            min = j * 2 + 1;
+void heapify(vector<int> &A, int i, int len) {
+    bool done = false;
+    while (!done) {
+        int max = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+
+        if (l < len && A[max] < A[l]) {
+            max = l;
+        }
+        if (r < len && A[max] < A[r]) {
+            max = r;
+        }
+        if (max == i) {
+            done = true;
+        }
+        swap(A[max], A[i]);
+        i = max;
+    }
+}
+
+void buildHeap(vector<int> &A, int len) {
+    for (int i = (len + 1) / 2 - 1; i >= 0; --i) {
+        heapify(A, i, len);
+    }
+}
+
+void heapSort(vector<int> &A) {
+    int len = A.size();
+    buildHeap(A, A.size());
+    for (int k = A.size() - 1; k > 0; --k) {
+        swap(A[0], A[k]);
+        --len;
+        heapify(A, 0, len);
+    }
+}
+
+void findKMin(vector<int> &A, int k) {
+    buildHeap(A, k);
+    for (int i = k; i < A.size(); ++i) {
+        if (A[i] < A[0]) {
+            swap(A[i], A[0]);
+            heapify(A, 0, k);
         }
     }
 }
 
-int findKMin(vector<int> &A, int k) {
-    int ans;
-    for (int i = A.size() - 1; i >= A.size() - k && i > 0; --i) {
-        heapify(A, i);
-        ans = A[0];
-        swap(A[0], A[i]);
-    }
-    return k == A.size() ? A[0] : ans;
-}
-
 int main() {
     vector<int> A;
+    generateVector(A, 10000000);
+    vector<int> B;
+    B = A;
     int k;
-    generateVector(A, 10);
+
     cin >> k;
-    for (int i : A) cout << i << " ";
-    cout << "\n";
-    cout << findKMin(A, k);
+
+    findKMin(A, k);
+    heapSort(B);
+
+    if (B[k - 1] == A[0]) cout << "Correct!";
+    else cout << "Wrong T_T";
 }
